08bea99603416240b72572000f5634a25032050d
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-28 11:39:26 -0500

    commit tree 0ca0afa3c18d9e0a1eb0f306d2ba9948c4755b0e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (self.a + self.b) <= self.c || (self.a + self.c) <= self.b || (self.b + self.c) <= self.a || self.a <= 0 || self.b <= 0 || self.c <= 0
+      raise TriangleError
+    else
+      if self.a == self.b && self.b == self.c
+        :equilateral
+      elsif self.a == self.b || self.b == self.c || self.c == self.a 
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Please try again."
+    end
+  end
+
 end
